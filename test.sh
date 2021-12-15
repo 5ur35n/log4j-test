7de6fda48b51bd3993c2bcbd05a0a30c1a471a43
5ur35n <88800354+5ur35n@users.noreply.github.com>
2021-12-15 17:53:37 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#This script is used to perform a fast check if your server is possibly affected by CVE-2021-44228 (the log4j vulnerability). 
-#It does not give a 100% proof, that you are not vulnerable, but it gives a hint if it is possible, that you could be vulnerable.
-#- Scans files for occurrences of log4j
-#- Checks for packages containing log4j and Solr ElasticSearch
-#- Checks if Java is installed
-#- Analyzes JAR/WAR/EAR files
-#- Option of checking hashes of .class files in archives
+# Make sure to run "sudo updatedb" prior to running the script.
+# How To Run Script: sudo wget https://raw.githubusercontent.com/5ur35n/log4j-test/main/test.sh -q -O - |bash
+
+# This script is used to perform a fast check if your server is possibly affected by CVE-2021-44228 (the log4j vulnerability). 
+# It does not give a 100% proof, that you are not vulnerable, but it gives a hint if it is possible, that you could be vulnerable.
+# - Scans files for occurrences of log4j
+# - Checks for packages containing log4j and Solr ElasticSearch
+# - Checks if Java is installed
+# - Analyzes JAR/WAR/EAR files
+# - Option of checking hashes of .class files in archives
 
 echo '################# Starting log4js-vuln-check #################'
 
